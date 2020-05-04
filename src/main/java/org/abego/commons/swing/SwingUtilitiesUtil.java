873ee3fc07cb02369186045833b7da471b67ea2d
Udo Borkowski <ub@abego-software.de>
2020-05-04 14:30:39 +0200

    New: add SwingUtilitiesUtil#evalInEDT/runAsyncInEDT

--- src/main/java/org/abego/commons/swing/SwingUtilitiesUtil.java
+++ src/main/java/org/abego/commons/swing/SwingUtilitiesUtil.java
@@ -1,20 +1,32 @@
 package org.abego.commons.swing;
 
+import org.abego.commons.lang.exception.MustNotInstantiateException;
+import org.abego.commons.var.Var;
+import org.abego.commons.var.VarUtil;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.function.Supplier;
+
 import static javax.swing.SwingUtilities.invokeAndWait;
+import static javax.swing.SwingUtilities.invokeLater;
 import static javax.swing.SwingUtilities.isEventDispatchThread;
 import static org.abego.commons.lang.exception.UncheckedException.newUncheckedException;
 
-import java.lang.reflect.InvocationTargetException;
-
-import org.abego.commons.lang.exception.MustNotInstantiateException;
-
 public final class SwingUtilitiesUtil {
-    
-	public static void runInEDT(Runnable runnable) {
+
+    /**
+     * Run the runnable in the Event Dispatch Thread.
+     *
+     * <p>The method will return when the runnable is finished .</p>
+     *
+     * <p>The method may also be called from the Event Dispatch Thread.</p>
+     */
+    public static void runInEDT(Runnable runnable) {
         if (isEventDispatchThread()) {
             runnable.run();
         } else {
             try {
+                //TODO: why invokeAndWait here? shouldn't this run async
                 invokeAndWait(runnable);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
@@ -25,7 +37,44 @@ public final class SwingUtilitiesUtil {
         }
     }
 
-	SwingUtilitiesUtil() {
+    /**
+     * Run the runnable in the Event Dispatch Thread.
+     *
+     * <p>The method may return before the runnable is finished, or has started.</p>
+     *
+     * <p>The method may also be called from the Event Dispatch Thread.</p>
+     */
+    public static void runAsyncInEDT(Runnable runnable) {
+        if (isEventDispatchThread()) {
+            runnable.run();
+        } else {
+            invokeLater(runnable);
+        }
+    }
+
+    /**
+     * Evaluate the expression in the Event Dispatch Thread and return the result.
+     *
+     * <p>The method may also be called from the Event Dispatch Thread.</p>
+     */
+    public static <T> T evalInEDT(Supplier<T> expression) {
+        if (isEventDispatchThread()) {
+            return expression.get();
+        }
+
+        Var<T> result = VarUtil.newVar();
+        try {
+            invokeAndWait(() -> result.set(expression.get()));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw newUncheckedException("Interrupted in evalInEDT", e); // NON-NLS
+        } catch (InvocationTargetException e) {
+            throw newUncheckedException("Error in evalInEDT", e); // NON-NLS
+        }
+        return result.get();
+    }
+
+    SwingUtilitiesUtil() {
         throw new MustNotInstantiateException();
     }
 
