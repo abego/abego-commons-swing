f24d19c262caf6cef889f39b6cabf735ac542489
Udo Borkowski <ub@abego-software.de>
2021-11-24 21:23:02 +0100

    new: add JComponentUtil#onJComponentBecomesVisible

--- src/main/java/org/abego/commons/swing/JComponentUtil.java
+++ src/main/java/org/abego/commons/swing/JComponentUtil.java
@@ -4,8 +4,10 @@ import org.abego.commons.lang.exception.MustNotInstantiateException;
 import org.eclipse.jdt.annotation.Nullable;
 
 import javax.swing.JComponent;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 
-final public class JComponentUtil {
+public final class JComponentUtil {
 
     JComponentUtil() {
         throw new MustNotInstantiateException();
@@ -16,4 +18,25 @@ final public class JComponentUtil {
             component.setBorder(null);
         }
     }
+
+    public static void onJComponentBecomesVisible(JComponent component, Runnable code) {
+        component.addAncestorListener(new AncestorListener() {
+            @Override
+            public void ancestorAdded(AncestorEvent event) {
+                code.run();
+            }
+
+            @Override
+            public void ancestorRemoved(AncestorEvent event) {
+
+            }
+
+            @Override
+            public void ancestorMoved(AncestorEvent event) {
+
+            }
+        });
+    }
+
+
 }
