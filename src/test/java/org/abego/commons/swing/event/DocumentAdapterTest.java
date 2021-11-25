d5f39c7d36eed5fa4d8d0a7d959a3c7e26126479
Udo Borkowski <ub@abego-software.de>
2021-11-25 07:47:00 +0100

    Int: improved test coverage

--- src/test/java/org/abego/commons/swing/event/DocumentAdapterTest.java
+++ src/test/java/org/abego/commons/swing/event/DocumentAdapterTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,7 +17,7 @@ class DocumentAdapterTest {
         StringBuilder log = new StringBuilder();
         JTextField tf = new JTextField();
 
-        tf.getDocument().addDocumentListener(new DocumentAdapter() {
+        DocumentAdapter listener = new DocumentAdapter() {
             @Override
             public void someUpdate(DocumentEvent e) {
                 log.append(String.format("%s %d %d\n",
@@ -23,16 +25,50 @@ class DocumentAdapterTest {
                         e.getOffset(),
                         e.getLength()));
             }
-        });
+        };
+        tf.getDocument().addDocumentListener(listener);
+
         tf.setText("foo");
         tf.setText("foobar");
-        tf.getDocument().remove(2,2);
+        tf.getDocument().remove(2, 2);
+
+        listener.changedUpdate(newChangedEventSample());
 
         assertEquals("INSERT 0 3\n" +
-                "REMOVE 0 3\n" +
-                "INSERT 0 6\n" +
-                "REMOVE 2 2\n",
+                        "REMOVE 0 3\n" +
+                        "INSERT 0 6\n" +
+                        "REMOVE 2 2\n" +
+                        "CHANGE 1 2\n",
                 log.toString());
+    }
+
+    private DocumentEvent newChangedEventSample() {
+        return new DocumentEvent() {
+            @Override
+            public int getOffset() {
+                return 1;
+            }
+
+            @Override
+            public int getLength() {
+                return 2;
+            }
+
+            @Override
+            public Document getDocument() {
+                return null;
+            }
+
+            @Override
+            public EventType getType() {
+                return EventType.CHANGE;
+            }
+
+            @Override
+            public ElementChange getChange(Element elem) {
+                return null;
+            }
+        };
     }
 
 }
